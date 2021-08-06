76e4a00b664f52ebbf8e06f2ffce27eebd378bd0
Derek Dhammaloka <derek_dhammaloka@yahoo.com>
2021-08-06 14:15:21 +0000

    Added componentDidMount at App.js

--- fcc-forum/src/App.js
+++ fcc-forum/src/App.js
@@ -44,25 +44,37 @@ function Header() {
   )
 }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  constructor(props) {
+    super(props)
+  }
+
+  state = {
+    users: [],
+    topics: [],
+    isFetching: false
+  }
+
+  findUser = (id) => {
+    return this.state.users.find(user => {
+      if (user.id===id) {
+        return user.avatar_template
+      }
+    })
+  }
+
+  async componentDidMount() {
+    const response = await fetch('https://forum-proxy.freecodecamp.rocks/latest')
+    const data = await response.json()
+    this.setState({
+      topics: data.topic_list.topics,
+      users: data.users,
+      isFetching: true
+    })
+  }
+
+  
 }
 
 export default App;
