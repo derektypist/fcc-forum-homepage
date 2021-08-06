f96cc5b1dcb733f8bd9da122f921136ecb03e40b
Derek Dhammaloka <derek_dhammaloka@yahoo.com>
2021-08-06 11:18:23 +0000

    Added Topic function at App.js

--- fcc-forum/src/App.js
+++ fcc-forum/src/App.js
@@ -1,5 +1,22 @@
 import React, {Component} from 'react';
 
+// Function for Topic
+function Topic(props) {
+  let url=`https://www.freecodecamp.org/forum/t/${props.id}`;
+  let imgUrl = "https://www.freecodecamp.org";
+  let profile = "https://www.freecodecamp.org/forum/u/";
+  return(
+    <div className="topic">
+      <div className="first">
+        <div>
+          <span>{props.index}</span>
+          <span><a href={url} target="_blank">{props.topic}</a></span>
+        </div>
+        <span>{props.images.map((index,key) => {})}</span>
+      </div>
+    </div>
+  )
+}
 function App() {
   return (
     <div className="App">
